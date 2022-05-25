c84268cd883336cd9c4a3393cb147b67219d0e30
Alex Gipi <alexgp895@gmail.com>
2022-05-25 15:53:01 +0200

    cache default ttl updated

--- app/middleware/cache.js
+++ app/middleware/cache.js
@@ -2,7 +2,7 @@ import getExpeditiousCache from 'express-expeditious';
 
 const defaultOptions = {
     namespace: 'expresscache',
-    defaultTtl: '10 minutes',
+    defaultTtl: '1 minute',
     statusCodeExpires: {
         404: '5 minutes',
         500: 0
